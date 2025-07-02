5670840268079693c74510709e56bc5260a7c430
4Foshen <dk4ction@mail.ru>
2025-07-02 21:53:21 +0500

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,14 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+
+  {
+    rules: {
+      "react/no-unescaped-entities": "off", // 🔧 отключаем ошибку кавычек
+    },
+  },
+];
 
 export default eslintConfig;
