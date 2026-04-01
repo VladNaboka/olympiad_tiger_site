3b8ad47e3154e8700fe91d6ed2511128f1faa3e2
4Foshen <120469505+4Foshen@users.noreply.github.com>
2026-04-01 20:34:11 +0500

    upd

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://tigersedu.com:8080/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
